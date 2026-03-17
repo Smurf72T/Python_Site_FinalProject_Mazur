fc36bd3fce15402de266db718ebb4209c9907952
Smurf72T <myskkk@yandex.ru>
2026-03-17 23:43:22 +0500

    админка

--- apps/ads/admin.py
+++ apps/ads/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Ad, Category, Review
 
-# Register your models here.
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = ('title', 'owner', 'status', 'created_at')
+    list_filter = ('status', 'category')
+    search_fields = ('title', 'description')
+
+admin.site.register(Category)
+admin.site.register(Review)
