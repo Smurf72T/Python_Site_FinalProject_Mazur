e6c8e749328bb848e86f9034032472e2fb9201c4
Smurf72T <myskkk@yandex.ru>
2026-03-17 23:27:58 +0500

    Формы регистрации и профиля, объявлений и отзывов

---
+++ apps/ads/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Ad, Review
+
+class AdForm(forms.ModelForm):
+    class Meta:
+        model = Ad
+        fields = ('title', 'description', 'price', 'location', 'image', 'category')
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = ('rating', 'comment')
